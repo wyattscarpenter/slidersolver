5af63d2a553fd7a8bc544ae6e3811966658b7a2c
wyattscarpenter <wyattscarpenter@gmail.com>
2018-08-20 07:01:42 -0500

    things aren't looking good for the solvability of this puzzle.

--- slidersolver2x3.py
+++ slidersolver2x3.py
@@ -16,18 +16,33 @@ assert(board == l(l(l(board))) and board == r(r(r(board))) and board != l(r(boar
        
 reachable_states = []
 paths = {}
+gen = [board]
+next_gen = []
 def f(b,p):
     #f is short for file, but like filing away, not a digital file
+    #print(p)
     if b in reachable_states:
-        pass
+        return False
     elif b == s:
+        print("We did it, Reddit!")
         print(p)
         exit()
     else:
         #print(b)
         reachable_states.append(b)
         paths[b] = p
-        f(r(b),p+'r')
-        f(l(b),p+'l')
+        return True
+
 f(board, '')
+while len(gen) != 0:
+    print(gen)
+    next_gen = []
+    for b in gen:
+        f(b,paths[b])
+        if f(r(b),paths[b]+'r'):
+            next_gen.append(r(b))
+        if f(l(b),paths[b]+'l'):
+            next_gen.append(l(b))
+    gen = next_gen
 print(reachable_states)
+print(len(reachable_states))
