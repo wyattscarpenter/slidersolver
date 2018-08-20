3abcdfae6c4f300a1f4470a91594b4a2aed00225
wyattscarpenter <wyattscarpenter@gmail.com>
2018-08-20 12:57:30 -0500

    made it general purpose
    now the search space is too large

--- slidersolver2x3.py
+++ slidersolver2x3.py
@@ -1,48 +1,59 @@
 # Solves a 2x3 slide puzzle
 
-# arrangement:
-# 0 1
-# 234
+h = 3
+w = 3
 
-board  = (0,2,1,3,4)
-s = (0,1,2,3,4)
-def l(b):
-    return (b[2],b[1],b[3],b[0],b[4])
+board  = (1,2,3,4,8,6,5,0,9)
+s = (1,2,3,4,0,5,6,7,8)
 
-def r(b):
-    return (b[0],b[4],b[2],b[1],b[3])
-
-assert(board == l(l(l(board))) and board == r(r(r(board))) and board != l(r(board)))
-       
+def v(b, i, j):
+    # b is a board, i is from, j is to (0-indexed!)
+    #012
+    #345
+    #678
+    return (b[j] == 0 and i >= 0 and j >= 0 and i < h*w and j<h*w and (
+        (j == i+1 and i+1%w != 0) or
+        (j == i+1 and i%w != 0) or
+        (j == i+w) or
+        (j == i-w)
+    ))
+def swappedcopy(b, i, j):
+    l = list(b)
+    tmp = l[j]
+    l[j] = l[i]
+    l[i] = tmp
+    return tuple(l)
 reachable_states = []
-paths = {}
+parents = {}
 gen = [board]
 next_gen = []
-def f(b,p):
+def f(b, parent=False):
+    global parents
     #f is short for file, but like filing away, not a digital file
-    #print(p)
+    #print(b)
     if b in reachable_states:
         return False
     elif b == s:
         print("We did it, Reddit!")
-        print(p)
         exit()
     else:
         #print(b)
         reachable_states.append(b)
-        paths[b] = p
+        parents[b] = parent
         return True
 
-f(board, '')
+f(board)
 while len(gen) != 0:
-    print(gen)
+    #print(gen)
     next_gen = []
     for b in gen:
-        f(b,paths[b])
-        if f(r(b),paths[b]+'r'):
-            next_gen.append(r(b))
-        if f(l(b),paths[b]+'l'):
-            next_gen.append(l(b))
+        for i, val in enumerate(b):
+            j = b.index(0)
+            if v(b, i, j):
+                c = swappedcopy(b,i,j)
+                f(c,b)
+                next_gen.append(c)
     gen = next_gen
+    print(len(reachable_states))
 print(reachable_states)
 print(len(reachable_states))
