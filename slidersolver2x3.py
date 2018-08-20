b2e97c68f5c2482ba72483af81db2f67cf4280dc
wyattscarpenter <wyattscarpenter@gmail.com>
2018-08-20 06:38:24 -0500

    infinite possibilities got me again

--- slidersolver2x3.py
+++ slidersolver2x3.py
@@ -4,28 +4,30 @@
 # 0 1
 # 234
 
-board  = [0,2,1,3,4]
-s = [0,1,2,3,4]
+board  = (0,2,1,3,4)
+s = (0,1,2,3,4)
 def l(b):
-    return [b[2],b[1],b[3],b[0],b[4]]
+    return (b[2],b[1],b[3],b[0],b[4])
 
 def r(b):
-    return [b[0],b[4],b[2],b[1],b[3]]
+    return (b[0],b[4],b[2],b[1],b[3])
 
 assert(board == l(l(l(board))) and board == r(r(r(board))) and board != l(r(board)))
        
-bs = [[board, ""]]
-while True:
-    tb = []
-    for b in bs:
-        print(b)
-        if b[0] == s:
-            print(b[1])
-            break
-        elif "rrr" in b[1] or "lll" in b[1]:
-            #do nothing, loop detected
-            print()
-        else:
-            tb.append([l(b[0]),b[1]+"l"])
-            tb.append([r(b[0]),b[1]+"r"])
-    bs = tb
+reachable_states = []
+paths = {}
+def f(b,p):
+    #f is short for file, but like filing away, not a digital file
+    if b in reachable_states:
+        pass
+    elif b == s:
+        print(p)
+        exit()
+    else:
+        #print(b)
+        reachable_states.append(b)
+        paths[b] = p
+        f(r(b),p+'r')
+        f(l(b),p+'l')
+f(board, '')
+print(reachable_states)
